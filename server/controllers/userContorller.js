93f91c0f0898c8f23a2a490cf22e31e1103dfb19
loretari <loretari@gmail.com>
2024-03-06 09:44:19 +0200

    init: fix buf in userController

--- server/controllers/userContorller.js
+++ server/controllers/userContorller.js
@@ -6,15 +6,13 @@ const userController = express.Router();
 
 // update user
 userController.put('/:id', async (req, res) => {
-    // first check the password is pass
+    try {
+        // first check the password is pass
     if (req.body.password) {
         req.body.password = await bcrypt.hash(req. body.password, 10);
     }
-    try {
-        const updateUser = await User.findByIdAndUpdate(req.params.id, {
-           //take everything inside req,body and set it again
-            $set: req.body
-        }, {new: true})
+        //take everything inside req,body and set it again
+        const updateUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
         return res.status(200).json(updateUser)
     } catch (error) {
         return res.status(500).json(error.message)
@@ -54,7 +52,7 @@ userController.get('/admin/find/:id', async (req, res) => {
 userController.post('/saveUser', async (req, res) => {
     try {
        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-       const newUser = await User.create({ ...res.body, password: hashedPassword })
+       const newUser = await User.create({ ...req.body, password: hashedPassword })
         return res.status(201).json(newUser);
     } catch (error) {
         return res.status(500).json(error.message)
