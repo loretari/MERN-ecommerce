e1b3862bf612023e09934a7da680527f4066791c
loretari <loretari@gmail.com>
2024-03-07 08:56:42 +0200

    init: add Slider in client side

---
+++ client/src/dummydata.js
@@ -0,0 +1,27 @@
+import Fashion1 from "./Assets/fashion14.1.jpg";
+import Fashion2 from "./Assets/fashion19.jpg";
+import Fashion3 from "./Assets/fashion7.1.jpg";
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: Fashion1,
+        title: "Start in the right way! ",
+        desc: "Whoever you’re shopping for this festive season, " +
+            "our comprehensive gift guide has a perfectly packaged " +
+            "present to suit all styles.",
+    },
+    {
+        id: 2,
+        img: Fashion2,
+        title: "Made with love",
+        desc: "Our editors reveal the leading looks on their radar for the new season.",
+    },
+    {
+        id: 3,
+        img: Fashion3,
+        title: "Stay fashionable",
+        desc: "With immaculate tailoring and superior artisanship, " +
+            "each dress has been thoughtfully crafted with the modern woman ",
+    }
+]
